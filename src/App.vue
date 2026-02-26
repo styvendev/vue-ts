99356d4b1e16ef7072640f541e9d24d8b50aabca
Styvendev <styven.dev@gmail.com>
2026-02-25 22:26:55 -0500

    new app in vue 3 and typescript

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <h1>Vue 3 + TypeScript Counter</h1>
+    <MyCounter />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MyCounter from './components/MyCounter.vue';
+</script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h3>Counter: {{ count }}</h3>
+    <h3>Square: {{ square }}</h3>
+    <div>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+const count = ref(0);
+const square = computed(() => count.value * count.value);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
