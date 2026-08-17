46239368d719055ca09ec742059817a8330cd36b
Angie Lorena Piracun Urquijo <styven.dev@gmail.com>
2026-08-16 23:07:36 -0500

    Added Tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>Contador</h1>
-  <!-- v-bind -->
-  <MyCounter :value="5" />
+  <div class="h-screen flex flex-col justify-center items-center">
+    <h1 class="text-xl font-bold text-blue-900">Contador</h1>
+    <!-- v-bind -->
+    <MyCounter :value="5" />
+    <MyCounter :value="5" />
+  </div>
 </template>
 
 <script lang="ts" setup>
--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -1,27 +1,27 @@
 <template>
-  <section>
-    <h3>Contador {{ count }}</h3>
-    <h3>Cuadrado {{ squared }}</h3>
+  <section class="text-center m-5">
+    <h3 class="text-lg">Valor {{ count }}</h3>
+    <h3 class="text-lg">Cuadrado {{ squared }}</h3>
     <!-- v-on:click -->
-    <button @click="count++">+1</button>
-    <button @click="count--">-1</button>
+    <button class="btn" @click="count++">+1</button>
+    <button class="btn" @click="count--">-1</button>
   </section>
 </template>
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue';
 
-/* Typescript */
-interface Props {
-  value: number;
-}
-const props = defineProps<Props>();
-
 /* JS
 const props = defineProps({
   value: { type: Number, required: true },
 });
 */
+
+/* Typescript */
+interface Props {
+  value: number;
+}
+const props = defineProps<Props>();
 
 /* Variable reactiva */
 const count = ref(props.value);
